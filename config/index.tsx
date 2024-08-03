9d198cd80d0265ac17b1b71be435b8c1c1622bfb
Sharan2922 <126399712+Sharan2922@users.noreply.github.com>
2024-08-03 19:31:56 +0530

    starter file

---
+++ config/index.tsx
@@ -0,0 +1,34 @@
+import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
+
+import { cookieStorage, createStorage } from 'wagmi'
+import { mainnet, sepolia } from 'wagmi/chains'
+
+// Get projectId from https://cloud.walletconnect.com
+export const projectId = "3f0b22504b0f467d443524b4208a3d06"
+
+if (!projectId) throw new Error('Project ID is not defined')
+
+export const metadata = {
+  name: 'Web3Modal',
+  description: 'Web3Modal Example',
+  url: 'https://web3modal.com', // origin must match your domain & subdomain
+  icons: ['https://avatars.githubusercontent.com/u/37784886']
+}
+
+// Create wagmiConfig
+const chains = [mainnet, sepolia] as const
+export const config = defaultWagmiConfig({
+  chains,
+  projectId,
+  metadata,
+  auth: {
+    email: true, // default to true
+    socials: ['google', 'x', 'github', 'discord', 'apple', 'facebook', 'farcaster'],
+    showWallets: true, // default to true
+    walletFeatures: true // default to true
+  },
+  ssr: true,
+  storage: createStorage({
+    storage: cookieStorage
+  }),
+})
